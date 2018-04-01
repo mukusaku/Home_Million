d078928b1471ed6bc36043460a1f9b5150ca6d14
mukusaku <mukusakuproducer@gmail.com>
2018-04-01 17:56:37 +0900

    初回コミット

---
+++ app/src/main/java/com/example/mukusaku/home_million/AppDetail.java
@@ -0,0 +1,13 @@
+package com.example.mukusaku.home_million;
+
+/**
+ * Created by Mukusaku on 2018/03/29.
+ */
+
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
